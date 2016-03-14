95b0587bc67fcf938b69fd80012f74fcceb25590
Berezovyy Vasyl <berezoviy.vasil@gmail.com>
2016-03-14 11:15:23 +0200

    Added initialization react-tap-event-plugin for material-ui components

--- src/views/AppView/AppView.js
+++ src/views/AppView/AppView.js
@@ -4,6 +4,11 @@ import { connect } from 'react-redux'
 import {} from '../../redux/modules/pricepinmap.js'
 // import classes from './AppView.scss'
 
+// Some components use react-tap-event-plugin to listen for touch events
+// So, we must inject this plugin at the start of our app.
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
 // // We use Flow (http://flowtype.org/) to type our component's props
 // and state. I've included both regular propTypes and
 // Flow types, but if you want to try just using Flow you'll want to
