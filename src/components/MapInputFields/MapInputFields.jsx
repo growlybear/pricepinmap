a2ef9b7444869fa15d96954b6c44d980542e69fc
Berezovyy Vasyl <berezoviy.vasil@gmail.com>
2016-03-14 18:00:11 +0200

    Added value handlers.

--- src/components/MapInputFields/MapInputFields.jsx
+++ src/components/MapInputFields/MapInputFields.jsx
@@ -11,16 +11,59 @@ export default class MapInputFields extends React.Component {
   static propTypes = {
   };
 
+  constructor () {
+    super()
+    this.state = {
+      'addressObject': {},
+      'soldPrice': '',
+      'soldDate': ''
+    }
+  }
+  // Since there is no particular event associated with the change the first argument
+  // will always be null and the second argument will be the new Date instance.
+  soldDateHandler (e, newDate) {
+    this.setState({
+      'soldDate': newDate
+    })
+  }
+
+  soldPriceHandler (e) {
+    this.setState({
+      'soldPrice': e.target.value
+    })
+  }
+
+  suggestAddressHandler (value) {
+    this.setState({
+      'addressObject': value
+    })
+  }
+
   render () {
     const fieldStyle = {
       'width': '100px'
     }
     return (
       <Paper className={classes.fieldsContainer} zDepth={5}>
-        <Geosuggest className={classes.geosuggest}/>
-        <span className={classes.inputField}><DatePicker textFieldStyle={fieldStyle} hintText='Sold date' /></span>
-        <span className={classes.inputField}><TextField style={fieldStyle} hintText='Sold price' type='number' /></span>
-        <span className={classes.inputField}><RaisedButton label='Add' secondary /></span>
+        <Geosuggest className={classes.geosuggest} onSuggestSelect={::this.suggestAddressHandler}/>
+        <span className={classes.inputField}>
+          <DatePicker
+            textFieldStyle={fieldStyle}
+            hintText='Sold date'
+            onChange={::this.soldDateHandler}
+            />
+        </span>
+        <span className={classes.inputField}>
+          <TextField
+            style={fieldStyle}
+            hintText='Sold price'
+            type='number'
+            onChange={::this.soldPriceHandler}
+            />
+        </span>
+        <span className={classes.inputField}>
+          <RaisedButton label='Add' secondary />
+        </span>
       </Paper>
     )
   }
