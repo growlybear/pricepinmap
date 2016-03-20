2d950c5b2c2c18a2748669f346b0f8166d1f74e9
Berezovyy Vasyl <berezoviy.vasil@gmail.com>
2016-03-20 20:20:49 +0200

    Added feature to cover all pins on the map.

--- src/components/MapLayout/MapLayout.jsx
+++ src/components/MapLayout/MapLayout.jsx
@@ -3,29 +3,29 @@ import React, { PropTypes } from 'react'
 import classes from './MapLayout.scss'
 import GoogleMap from 'google-map-react'
 import MapInputFields from 'components/MapInputFields/MapInputFields.jsx'
+import { fitBounds } from 'google-map-react/utils'
 import Pin from './Pin/Pin.jsx'
 
 export default class MapLayout extends React.Component {
-  static defaultProps = {
-    center: {lat: 0, lng: 0},
-    zoom: 1
-  };
 
   static propTypes = {
-    center: PropTypes.object.isRequired,
     addPin: PropTypes.func.isRequired,
     handleClickPinMap: PropTypes.func.isRequired,
-    zoom: PropTypes.number.isRequired,
     pinObjects: PropTypes.array
   };
 
   handlePinClick (e) {
+    // Return id of pin that was clicked to AppView
+    // So, we will can show/hide detailed information of pin.
     this.props.handleClickPinMap(e)
   }
 
   render () {
-    const pins = []
+    // If there is no pins - we set map size to full screen and hide part with list of pins.
     let style = classes.fullScreen
+    // Next, we want to render all added pins to the map.
+    // Also, if there is at least one pins - we change map size.
+    const pins = []
     if (this.props.pinObjects.length !== 0) {
       this.props.pinObjects.map((i) => {
         pins.push(
@@ -39,8 +39,58 @@ export default class MapLayout extends React.Component {
       })
       style = classes.leftScreen
     }
+
+    // We will use https://github.com/istarkov/google-map-react#fitbounds-func
+    // To dynamically fit bounds on the map.
+    // For it we need two geometry corners(NW, SE) and size in px of the map
+    // It will return coordinates of the center and the value of zoom.
+
+    // First of all we need to get all dots of all directions from pins.
+    const allBounds = {
+      's': [],
+      'n': [],
+      'w': [],
+      'e': []
+    }
+    // We need to declare default value for zoom and center
+    // if there are no pins on the map.
+    let zoom = 1
+    let center = {
+      lat: 0,
+      lng: 0
+    }
+    // Next, we must check if there is at least one pin on the map
+    // And if it's return true - we must get bounds and zoom for that pin.
+    if (this.props.pinObjects.length) {
+      // Get all dots of all directions
+      this.props.pinObjects.map((i) => {
+        allBounds.s.push(Number(i.addressObject.gmaps.geometry.bounds.south))
+        allBounds.n.push(Number(i.addressObject.gmaps.geometry.bounds.north))
+        allBounds.w.push(Number(i.addressObject.gmaps.geometry.bounds.west))
+        allBounds.e.push(Number(i.addressObject.gmaps.geometry.bounds.east))
+      })
+      // Get coordinates of two geometry corners that will cover all pins
+      const nw = {
+        lat: Math.max.apply(null, allBounds.n),
+        lng: Math.min.apply(null, allBounds.w)
+      }
+      const se = {
+        lat: Math.min.apply(null, allBounds.s),
+        lng: Math.max.apply(null, allBounds.e)
+      }
+      // We get must get width/height from refs because there are different size of screens
+      // And we cann't set it statically.
+      let size = {
+        width: this.refs.googleMap.clientWidth,
+        height: this.refs.googleMap.clientHeight
+      }
+      // Get bounds that cover all pins and change default value for center and zoom.
+      const bounds = fitBounds({nw, se}, size)
+      center = bounds.center
+      zoom = bounds.zoom
+    }
     return (
-      <div className={style}>
+      <div className={style} ref='googleMap'>
         <MapInputFields
           addPin={this.props.addPin}
         />
@@ -48,14 +98,14 @@ export default class MapLayout extends React.Component {
           bootstrapURLKeys={{
             key: 'AIzaSyB6mLuq06WnR7VV9A_Hzu19bxwhrTv3nDs'
           }}
-          defaultCenter={this.props.center}
-          onChildClick={::this.handlePinClick}
-          defaultZoom={this.props.zoom}>
-         {pins.map((i) => {
-           return (
-              i
-            )
-         }, this)}
+          center={center}
+          zoom={zoom}
+          onChildClick={::this.handlePinClick}>
+           {pins.map((i) => {
+             return (
+                i
+              )
+           }, this)}
         </GoogleMap>
       </div>
     )
